67c0260e7badbfa5e616a41ee05df35d928d341c
Adelnors <erlando.putra.21@gmail.com>
2024-06-03 17:37:05 +0700

    refractor: changing the port for cloud run compatibility

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use('/', require('./routes/routes'))
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000')
+app.listen(8080, () => {
+    console.log('Server is running on port 8080')
 })
