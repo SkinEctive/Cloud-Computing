4efb1ef7b3835d2e6c0ce08d82977ca2a780ccb7
Adelnors <erlando.putra.21@gmail.com>
2024-06-03 17:37:31 +0700

    fix: fixing database bugs

--- prisma/migrations/20240602143213_init_tables/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Made the column `isAdmin` on table `user` required. This step will fail if there are existing NULL values in that column.
-
-*/
--- AlterTable
-ALTER TABLE `user` MODIFY `isAdmin` BOOLEAN NOT NULL DEFAULT false;
